2ba134c8dc233fcfe622c8614d255b842bbc9ee7
gjr80 <gjroderick@gmail.com>
2020-07-07 23:30:48 +1000

    One more try to fix the calc_trend None bug

--- bin/user/rtgd.py
+++ bin/user/rtgd.py
@@ -2754,7 +2754,7 @@ def calc_trend(obs_type, now_vt, group, db_manager, then_ts, grace=0):
             now = convert(now_vt, group).value
             then = convert(then_vt, group).value
             # we know now is not None but what of then?
-            if then_vt is not None:
+            if then is not None:
                 return now - then
             else:
                 return None
